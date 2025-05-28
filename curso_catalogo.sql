f835da204019a5483a2441af0c55c233c1d3ab49
BrazenGhost630 <fel.vargaso@duocuc.cl>
2025-05-28 17:42:52 -0400

    Update curso_catalogo.sql

--- curso_catalogo.sql
+++ curso_catalogo.sql
@@ -1,15 +1,15 @@
 
 
-INSERT INTO `curso_catalogo` (`activo`, `duracion_horas`, `id`, `descripcion`, `categoria`, `nombre`,`cuposDisponibles`, `coste`) VALUES
-(b'0', 40, 1, 'Juegos AAA', 'Juegos', 'Juegos A gran escala','20'),
-(b'1', 35, 2, 'Fundamentos de Inteligencia Artificial', 'Tecnología', 'Introducción a IA',20,20000),
-(b'1', 50, 3, 'Diseño de interfaces de usuario', 'Diseño', 'UI/UX para aplicaciones','20',25000),
-(b'0', 40, 4, 'Producción musical con herramientas digitales', 'Arte', 'Composición digital',20,27000),
-(b'1', 45, 5, 'Gestión de bases de datos con MySQL', 'Tecnología', 'Administración de DB',20,24000),
-(b'1', 30, 6, 'Marketing digital y redes sociales', 'Negocios', 'Estrategias de crecimiento',20,20000),
-(b'0', 55, 7, 'Programación en Python desde cero', 'Programación', 'Python básico','20',20000),
-(b'1', 48, 8, 'Seguridad informática y hacking ético', 'Tecnología', 'Ciberseguridad avanzada',20,20000),
-(b'1', 25, 9, 'Arte digital con Photoshop e Illustrator', 'Diseño', 'Ilustración digital',20,20000),
-(b'0', 20, 10, 'Creación de contenido para YouTube', 'Comunicación', 'YouTube para principiantes',20,20000),
-(b'1', 60, 11, 'Desarrollo de juegos AAA', 'Juegos', 'Creación de Juegos A gran escala',20,20000);
+INSERT INTO `curso_catalogo` (`activo`, `duracion_horas`, `descripcion`, `categoria`, `nombre`,`cupos_Disponible`, `coste`) VALUES
+(b'0', 40, 'Juegos AAA', 'Juegos', 'Juegos A gran escala',20,20000),
+(b'1', 35, 'Fundamentos de Inteligencia Artificial', 'Tecnología', 'Introducción a IA',20,20000),
+(b'1', 50, 'Diseño de interfaces de usuario', 'Diseño', 'UI/UX para aplicaciones',20,25000),
+(b'0', 40, 'Producción musical con herramientas digitales', 'Arte', 'Composición digital',20,27000),
+(b'1', 45, 'Gestión de bases de datos con MySQL', 'Tecnología', 'Administración de DB',20,24000),
+(b'1', 30, 'Marketing digital y redes sociales', 'Negocios', 'Estrategias de crecimiento',20,20000),
+(b'0', 55, 'Programación en Python desde cero', 'Programación', 'Python básico',20,20000),
+(b'1', 48, 'Seguridad informática y hacking ético', 'Tecnología', 'Ciberseguridad avanzada',20,20000),
+(b'1', 25, 'Arte digital con Photoshop e Illustrator', 'Diseño', 'Ilustración digital',20,20000),
+(b'0', 20,  'Creación de contenido para YouTube', 'Comunicación', 'YouTube para principiantes',20,20000),
+(b'1', 60,  'Desarrollo de juegos AAA', 'Juegos', 'Creación de Juegos A gran escala',20,20000);
 
